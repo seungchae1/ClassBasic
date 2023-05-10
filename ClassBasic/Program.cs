f898b607a4416c69ed7d2f5ccb107a793c7b59cd
Seungchae Park <s2125@e-mirim.hs.kr>
2023-05-10 12:55:23 +0900

    초기 커밋.

---
+++ ClassBasic/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program234
+    {
+        static void Main(string[] args)
+        {
+            OtherClass oc = new OtherClass();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
